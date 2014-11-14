6a640597a3dfbd18a43caf2f7edf721e825b53f1
Shawn Anderson <shawn42@gmail.com>
2014-11-14 18:26:45 -0500

    Fuhzee bot and build

---
+++ robots/gvsu/Fuhzee.java
@@ -0,0 +1,58 @@
+package gvsu;
+import robocode.*;
+import java.awt.Color;
+
+// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+
+/**
+ * Fuhzee - a robot by shawn42
+ */
+public class Fuhzee extends Robot
+{
+	/**
+	 * run: Fuhzee's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(Math.ceil(Math.random())*100);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
+}
