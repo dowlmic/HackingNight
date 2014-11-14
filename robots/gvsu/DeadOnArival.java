f96f12ab806925b5895cbad7531e51a6315a6d26
Spencer GVSU <amanns@mail.gvsu.edu>
2014-11-14 18:49:48 -0500

    defense is the best

--- robots/gvsu/DeadOnArival.java
+++ robots/gvsu/DeadOnArival.java
@@ -1,6 +1,6 @@
 package gvsu;
 import robocode.*;
-//import java.awt.Color;
+import java.awt.Color;
 
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
 
@@ -9,6 +9,9 @@ import robocode.*;
  */
 public class DeadOnArival extends Robot
 {
+	int turnDirection = 1; // Clockwise or counterclockwise
+	boolean peek; // Don't turn if there's a robot there
+	double moveAmount; // How much to move
 	/**
 	 * run: DeadOnArival's default behavior
 	 */
@@ -18,15 +21,36 @@ public class DeadOnArival extends Robot
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
 
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+	//	setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+		setBodyColor(Color.black);
+		setGunColor(Color.red);
+		setRadarColor(Color.red);
+		setBulletColor(Color.red);
+		setScanColor(Color.orange);
+		
+	// Initialize moveAmount to the maximum possible for this battlefield.
+		moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
+		// Initialize peek to false
+		peek = false;
 
-		// Robot main loop
-		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+		// turnLeft to face a wall.
+		// getHeading() % 90 means the remainder of
+		// getHeading() divided by 90.
+		turnLeft(getHeading() % 90);
+		ahead(moveAmount);
+		// Turn the gun to turn right 90 degrees.
+		peek = true;
+		turnGunRight(90);
+		turnRight(90);
+		while (true) {
+			// Look before we turn when ahead() completes.
+			peek = true;
+			// Move up the wall
+			ahead(moveAmount);
+			// Don't look now
+			peek = false;
+			// Turn to the next wall
+			turnRight(90);
 		}
 	}
 
@@ -34,9 +58,23 @@ public class DeadOnArival extends Robot
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(1);
+
+		if (e.getBearing() >= 0) {
+			turnDirection = 1;
+		} else {
+			turnDirection = -1;
+		}
+
+		
+		if (e.getDistance() <= moveAmount*2) {
+			ahead(e.getDistance() + 5);
+			turnRight(e.getBearing());
+		}
+		
+		
+		scan(); // Might want to move ahead again!
 	}
+
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
@@ -53,4 +91,28 @@ public class DeadOnArival extends Robot
 		// Replace the next line with any behavior you would like
 		back(20);
 	}	
+	
+	public void onHitRobot(HitRobotEvent e) {
+		if (e.getBearing() >= 0) {
+			turnDirection = 1;
+		} else {
+			turnDirection = -1;
+		}
+		turnRight(e.getBearing());
+
+		// Determine a shot that won't kill the robot...
+		// We want to ram him instead for bonus points
+		if (e.getEnergy() > 16) {
+			fire(3);
+		} else if (e.getEnergy() > 10) {
+			fire(2);
+		} else if (e.getEnergy() > 4) {
+			fire(1);
+		} else if (e.getEnergy() > 2) {
+			fire(.5);
+		} else if (e.getEnergy() > .4) {
+			fire(.1);
+		}
+		ahead(40); // Ram him again!
+	}
 }
