d517bd954cfb121faff5ba7ed02c908be02dba92
dowlmic <dowlmic@gmail.com>
2014-11-14 22:06:36 -0500

    Edit Steve

--- robots/gvsu/Steve.java
+++ robots/gvsu/Steve.java
@@ -1,56 +1,147 @@
+/*******************************************************************************
+ * Copyright (c) 2001-2014 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://robocode.sourceforge.net/license/epl-v10.html
+ *******************************************************************************/
 package gvsu;
-import robocode.*;
-//import java.awt.Color;
 
-// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+
+import robocode.*;
+import robocode.Robot;
+
+import java.awt.*;
+
+import static robocode.util.Utils.normalRelativeAngleDegrees;
+
+import java.awt.*;
 
 /**
- * Steve - a robot by (your name here)
+ * Crazy - a sample robot by Mathew Nelson.
+ * <p/>
+ * This robot moves around in a crazy pattern.
+ *
+ * @author Mathew A. Nelson (original)
+ * @author Flemming N. Larsen (contributor)
  */
-public class Steve extends Robot
-{
+public class Steve extends AdvancedRobot {
+	boolean movingForward;
+
+	
 	/**
-	 * run: Steve's default behavior
+	 * run: Crazy's main run function
 	 */
 	public void run() {
-		// Initialization of the robot should be put here
+	setAdjustGunForRobotTurn(true);setAdjustGunForRobotTurn(true);
+		// Set colors
+		setBodyColor(new Color(0, 200, 0));
+		setGunColor(new Color(0, 150, 50));
+		setRadarColor(new Color(0, 100, 100));
+		setBulletColor(new Color(255, 255, 100));
+		setScanColor(new Color(255, 200, 200));
 
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
-
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
-
-		// Robot main loop
-		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+		// Loop forever
+		while (true) {
+			setTurnGunRight(500);
+			// Tell the game we will want to move ahead 40000 -- some large number
+			setAhead(20000);
+			movingForward = true;
+			// Tell the game we will want to turn right 90
+			setTurnRight(90);
+			avoidWall();
+			// At this point, we have indicated to the game that *when we do something*,
+			// we will want to move ahead and turn right.  That's what "set" means.
+			// It is important to realize we have not done anything yet!
+			// In order to actually move, we'll want to call a method that
+			// takes real time, such as waitFor.
+			// waitFor actually starts the action -- we start moving and turning.
+			// It will not return until we have finished turning.
+			waitFor(new TurnCompleteCondition(this));
+			setTurnGunRight(500);
+			// Note:  We are still moving ahead now, but the turn is complete.
+			// Now we'll turn the other way...
+			setTurnLeft(180);
+			avoidWall();
+			// ... and wait for the turn to finish ...
+			waitFor(new TurnCompleteCondition(this));
+			setTurnGunRight(500);
+			// ... then the other way ...
+			setTurnRight(180);
+			avoidWall();
+			// .. and wait for that turn to finish.
+			waitFor(new TurnCompleteCondition(this));
+			// then back to the top to do it all again
+			
 		}
 	}
 
 	/**
-	 * onScannedRobot: What to do when you see another robot
+	 * onHitWall:  Handle collision with wall.
 	 */
-	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(1);
+	public void onHitWall(HitWallEvent e) {
+		// Bounce off!
+		reverseDirection();
 	}
 
 	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
+	 * reverseDirection:  Switch from ahead to back & vice versa
 	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
+	public void reverseDirection() {
+		if (movingForward) {
+			setBack(40000);
+			movingForward = false;
+		} else {
+			setAhead(40000);
+			movingForward = true;
+		}
 	}
-	
+	public void avoidWall(){
+		if (getX() < 200 || getY() < 200 || getBattleFieldWidth() - getX() < 200 ||
+			getBattleFieldHeight() - getY() < 200)
+			reverseDirection();
+	}
+
 	/**
-	 * onHitWall: What to do when you hit a wall
+	 * onScannedRobot:  Fire!
 	 */
-	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
-	}	
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Calculate exact location of the robot
+		double absoluteBearing = getHeading() + e.getBearing();
+		double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
+
+		// If it's close enough, fire!
+		if (Math.abs(bearingFromGun) <= 2) {
+			setTurnGunRight(bearingFromGun);
+			// We check gun heat here, because calling fire()
+			// uses a turn, which could cause us to lose track
+			// of the other robot.
+			if (getGunHeat() == 0) {
+				if (e.getDistance() < 50)
+					fire(Rules.MAX_BULLET_POWER);
+				else
+					fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
+			}
+		} // otherwise just set the gun to turn.
+		// Note:  This will have no effect until we call scan()
+		else {
+			setTurnGunRight(bearingFromGun);
+		}
+		// Generates another scan event if we see a robot.
+		// We only need to call this if the gun (and therefore radar)
+		// are not turning.  Otherwise, scan is called automatically.
+		if (bearingFromGun == 0) {
+			scan();
+		}
+	}
+
+	/**
+	 * onHitRobot:  Back up!
+	 */
+	public void onHitRobot(HitRobotEvent e) {
+		// If we're moving the other robot, reverse!
+		if (e.isMyFault()) {
+			reverseDirection();
+		}
+	}
 }
